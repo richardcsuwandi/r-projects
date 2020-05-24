8f12c507aa1942a3080f4f95fb9ae2a8e02602fb
richardcsuwandi <59959022+richardcsuwandi@users.noreply.github.com>
2020-05-24 17:29:07 +0700

    Update play_golf.R

--- Golf Weather Forecast Shiny App/play_golf.R
+++ Golf Weather Forecast Shiny App/play_golf.R
@@ -20,8 +20,8 @@ ui <- fluidPage(theme = shinytheme("simplex"),
   # Input values
   sidebarPanel(
     HTML("<h3>Input Parameters</h3>"),
-    
-    selectInput("outlook", label = "Outlook:", 
+
+    selectInput("outlook", label = "Outlook:",
                 choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
                 selected = "Rainy"),
     sliderInput("temperature", "Temperature:",
@@ -30,28 +30,27 @@ ui <- fluidPage(theme = shinytheme("simplex"),
     sliderInput("humidity", "Humidity:",
                 min = 65, max = 96,
                 value = 90),
-    selectInput("windy", label = "Windy:", 
-                choices = list("Yes" = "TRUE", "No" = "FALSE"), 
+    selectInput("windy", label = "Windy:",
+                choices = list("Yes" = "TRUE", "No" = "FALSE"),
                 selected = "TRUE"),
-    
+
     actionButton("submitbutton", "Submit", class = "btn btn-primary")
   ),
-  
+
   mainPanel(
     tags$label(h3('Output')), # Output text Box
     verbatimTextOutput('contents'),
     tableOutput('tabledata') # Prediction results table
-    
+
   )
 )
 
-
-# Server                           
+# Server
 server <- function(input, output, session) {
 
   # Input Data
-  datasetInput <- reactive({  
-    
+  datasetInput <- reactive({
+
   df <- data.frame(
     Name = c("outlook",
              "temperature",
@@ -62,38 +61,37 @@ server <- function(input, output, session) {
                            input$humidity,
                            input$windy)),
     stringsAsFactors = FALSE)
-  
+
   play <- "play"
   df <- rbind(df, play)
   input <- transpose(df)
   write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
-  
+
   test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
-  
+
   test$outlook <- factor(test$outlook, levels = c("overcast", "rainy", "sunny"))
-  
-  
+
   Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
   print(Output)
-  
+
   })
-  
+
   # Output text box
   output$contents <- renderPrint({
-    if (input$submitbutton>0) { 
-      isolate("Calculation complete.") 
+    if (input$submitbutton>0) {
+      isolate("Calculation complete.")
     } else {
       return("Server is ready for calculation.")
     }
   })
-  
+
   # Prediction results table
   output$tabledata <- renderTable({
-    if (input$submitbutton>0) { 
-      isolate(datasetInput()) 
-    } 
+    if (input$submitbutton>0) {
+      isolate(datasetInput())
+    }
   })
-  
+
 }
 
 # Create the shiny app
